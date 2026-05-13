451c47eecc3722f70cc33f9e7b5b891aaa495f6b
zainabsacid-svg <zainabsacid@gmail.com>
2026-05-14 00:00:19 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3B82F6",
+        dark: "#050505",
+      }
+    },
+  },
+  plugins: [],
+}
